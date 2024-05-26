fcf40a975e33d1b4d9b7c1cb918d9156738c6b9a
Maksym Rybalko <maxymrybalko2005@gmail.com>
2024-05-26 13:30:53 +0300

    Added balancer and balancer tests

--- cmd/lb/balancer.go
+++ cmd/lb/balancer.go
@@ -7,27 +7,33 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
-	"github.com/roman-mazur/architecture-practice-4-template/httptools"
-	"github.com/roman-mazur/architecture-practice-4-template/signal"
+	"github.com/KPI-3-Architecture-Labs/lab4/httptools"
+	"github.com/KPI-3-Architecture-Labs/lab4/signal"
 )
 
 var (
-	port = flag.Int("port", 8090, "load balancer port")
+	port       = flag.Int("port", 8090, "load balancer port")
 	timeoutSec = flag.Int("timeout-sec", 3, "request timeout time in seconds")
-	https = flag.Bool("https", false, "whether backends support HTTPs")
+	https      = flag.Bool("https", false, "whether backends support HTTPs")
 
 	traceEnabled = flag.Bool("trace", false, "whether to include tracing information into responses")
 )
 
 var (
-	timeout = time.Duration(*timeoutSec) * time.Second
+	timeout     = time.Duration(*timeoutSec) * time.Second
 	serversPool = []string{
 		"server1:8080",
 		"server2:8080",
 		"server3:8080",
 	}
+	poolOfHealthyServers = make([]string, len(serversPool))
+	poolLock             sync.Mutex
+
+	bytesServed     = make(map[string]int64)
+	bytesServedLock sync.Mutex
 )
 
 func scheme() string {
@@ -38,13 +44,21 @@ func scheme() string {
 }
 
 func health(dst string) bool {
-	ctx, _ := context.WithTimeout(context.Background(), timeout)
-	req, _ := http.NewRequestWithContext(ctx, "GET",
-		fmt.Sprintf("%s://%s/health", scheme(), dst), nil)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("%s://%s/health", scheme(), dst), nil)
+	if err != nil {
+		return false
+	}
+
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return false
 	}
+
+	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
 		return false
 	}
@@ -52,7 +66,8 @@ func health(dst string) bool {
 }
 
 func forward(dst string, rw http.ResponseWriter, r *http.Request) error {
-	ctx, _ := context.WithTimeout(r.Context(), timeout)
+	ctx, cancel := context.WithTimeout(r.Context(), timeout)
+	defer cancel()
 	fwdRequest := r.Clone(ctx)
 	fwdRequest.RequestURI = ""
 	fwdRequest.URL.Host = dst
@@ -60,46 +75,116 @@ func forward(dst string, rw http.ResponseWriter, r *http.Request) error {
 	fwdRequest.Host = dst
 
 	resp, err := http.DefaultClient.Do(fwdRequest)
-	if err == nil {
-		for k, values := range resp.Header {
-			for _, value := range values {
-				rw.Header().Add(k, value)
-			}
-		}
-		if *traceEnabled {
-			rw.Header().Set("lb-from", dst)
-		}
-		log.Println("fwd", resp.StatusCode, resp.Request.URL)
-		rw.WriteHeader(resp.StatusCode)
-		defer resp.Body.Close()
-		_, err := io.Copy(rw, resp.Body)
-		if err != nil {
-			log.Printf("Failed to write response: %s", err)
-		}
-		return nil
-	} else {
+	if err != nil {
 		log.Printf("Failed to get response from %s: %s", dst, err)
 		rw.WriteHeader(http.StatusServiceUnavailable)
 		return err
+	}
+	defer resp.Body.Close()
+
+	for k, values := range resp.Header {
+		for _, value := range values {
+			rw.Header().Add(k, value)
+		}
+	}
+
+	if *traceEnabled {
+		rw.Header().Set("lb-from", dst)
+	}
+
+	log.Println("fwd", resp.StatusCode, resp.Request.URL)
+	rw.WriteHeader(resp.StatusCode)
+
+	bytes, err := io.Copy(rw, resp.Body)
+	if err != nil {
+		log.Printf("Failed to write response: %s", err)
+	}
+
+	bytesServedLock.Lock()
+	bytesServed[dst] += bytes
+	bytesServedLock.Unlock()
+
+	return nil
+}
+
+func getSmallestTraffic() int {
+	bytesServedLock.Lock()
+	defer bytesServedLock.Unlock()
+
+	var minBytes int64 = -1
+	minIndex := -1
+
+	for i, server := range poolOfHealthyServers {
+		if server == "" {
+			continue
+		}
+		if minBytes == -1 || bytesServed[server] < minBytes {
+			minBytes = bytesServed[server]
+			minIndex = i
+		}
+	}
+
+	return minIndex
+}
+
+func getServer(index int) string {
+	poolLock.Lock()
+	defer poolLock.Unlock()
+	return poolOfHealthyServers[index]
+}
+
+func healthCheck(servers []string, result []string) {
+	healthStatus := make(map[string]bool)
+	for _, server := range servers {
+		healthStatus[server] = true
+	}
+
+	for i, server := range servers {
+		i := i
+		go func(server string) {
+			for range time.Tick(10 * time.Second) {
+				isHealthy := health(server)
+				poolLock.Lock()
+
+				if isHealthy {
+					healthStatus[server] = true
+					result[i] = server
+				} else {
+					healthStatus[server] = false
+					result[i] = ""
+				}
+
+				poolOfHealthyServers = nil
+
+				for _, server := range servers {
+					if healthStatus[server] {
+						poolOfHealthyServers = append(poolOfHealthyServers, server)
+					}
+				}
+
+				poolLock.Unlock()
+				log.Println(server, isHealthy)
+			}
+		}(server)
 	}
 }
 
 func main() {
 	flag.Parse()
 
-	// TODO: Використовуйте дані про стан сервреа, щоб підтримувати список тих серверів, яким можна відправляти ззапит.
-	for _, server := range serversPool {
-		server := server
-		go func() {
-			for range time.Tick(10 * time.Second) {
-				log.Println(server, health(server))
-			}
-		}()
-	}
+	healthCheck(serversPool, poolOfHealthyServers)
 
 	frontend := httptools.CreateServer(*port, http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		// TODO: Рееалізуйте свій алгоритм балансувальника.
-		forward(serversPool[0], rw, r)
+		serverIndex := getSmallestTraffic()
+		if serverIndex == -1 {
+			http.Error(rw, "No available servers", http.StatusServiceUnavailable)
+			return
+		}
+		dst := getServer(serverIndex)
+		err := forward(dst, rw, r)
+		if err != nil {
+			return
+		}
 	}))
 
 	log.Println("Starting load balancer...")
--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,7 +1,141 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"time"
 
-func TestBalancer(t *testing.T) {
-	// TODO: Реалізуйте юніт-тест для балансувальникка.
+	"github.com/stretchr/testify/assert"
+)
+
+func Test(t *testing.T) {
+	suite := new(TestSuite)
+	suite.SetupSuite()
+	t.Run("TestBalancer", suite.TestBalancer)
+	t.Run("TestHealth", suite.TestHealth)
+	t.Run("TestTraffic", suite.TestGetSmallestTraffic)
+	t.Run("TestTraffic2", suite.TestGetSmallestTrafficTwo)
+}
+
+type TestSuite struct {
+	serversPool []string
+}
+
+func (s *TestSuite) SetupSuite() {
+	s.serversPool = []string{
+		"server1:8080",
+		"server2:80",
+		"server3:80",
+	}
+}
+
+func (s *TestSuite) TestBalancer(t *testing.T) {
+	server1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server1.Close()
+
+	server2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server2.Close()
+
+	server3 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusServiceUnavailable)
+	}))
+	defer server3.Close()
+
+	parsedURL1, _ := url.Parse(server1.URL)
+	hostURL1 := parsedURL1.Host
+
+	parsedURL2, _ := url.Parse(server2.URL)
+	hostURL2 := parsedURL2.Host
+
+	parsedURL3, _ := url.Parse(server3.URL)
+	hostURL3 := parsedURL3.Host
+
+	serversPool = []string{
+		hostURL1,
+		hostURL2,
+		hostURL3,
+	}
+
+	poolOfHealthyServers = []string{
+		hostURL1,
+		hostURL2,
+	}
+
+	bytesServed = make(map[string]int64)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+
+	err := forward(hostURL1, w, req)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	bytesServedLock.Lock()
+	assert.GreaterOrEqual(t, bytesServed[hostURL1], int64(0))
+	bytesServedLock.Unlock()
+}
+
+func (s *TestSuite) TestGetSmallestTraffic(t *testing.T) {
+	poolOfHealthyServers = []string{"server1:8080", "server2:8080", "server3:8080"}
+	bytesServed = map[string]int64{
+		"server1:8080": 100,
+		"server2:8080": 50,
+		"server3:8080": 150,
+	}
+
+	expectedIndex := 1
+	actualIndex := getSmallestTraffic()
+	assert.Equal(t, expectedIndex, actualIndex)
+}
+
+func (s *TestSuite) TestGetSmallestTrafficTwo(t *testing.T) {
+	poolOfHealthyServers = []string{"server1:8080", "server2:8080", "server3:8080"}
+	bytesServed = map[string]int64{
+		"server1:8080": 1488,
+		"server2:8080": 5252,
+		"server3:8080": 228,
+	}
+
+	expectedIndex := 2
+	actualIndex := getSmallestTraffic()
+	assert.Equal(t, expectedIndex, actualIndex)
+}
+
+func (s *TestSuite) TestHealth(t *testing.T) {
+	result := make([]string, len(s.serversPool))
+
+	server1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server1.Close()
+
+	server2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server2.Close()
+
+	parsedURL1, _ := url.Parse(server1.URL)
+	hostURL1 := parsedURL1.Host
+
+	parsedURL2, _ := url.Parse(server2.URL)
+	hostURL2 := parsedURL2.Host
+
+	servers := []string{
+		hostURL1,
+		hostURL2,
+		"server3:8080",
+	}
+
+	healthCheck(servers, result)
+	time.Sleep(12 * time.Second)
+
+	assert.Equal(t, hostURL1, result[0])
+	assert.Equal(t, hostURL2, result[1])
+	assert.Equal(t, "", result[2])
 }
