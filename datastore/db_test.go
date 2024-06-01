31cfb69a59f51282ee3551ed25ff053a3981eace
Antonio <antony.dovzhenko@gmail.com>
2024-06-01 10:53:40 +0300

    Added basic tests

--- datastore/db_test.go
+++ datastore/db_test.go
@@ -7,42 +7,57 @@ import (
 	"testing"
 )
 
-func TestDb_Put(t *testing.T) {
+type Data struct {
+	key   string
+	value string
+}
+
+func TestPutAndGet(t *testing.T) {
 	dir, err := ioutil.TempDir("", "test-db")
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer os.RemoveAll(dir)
 
-	db, err := NewDb(dir)
+	db, err := NewDb(dir, 333)
+
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer db.Close()
 
-	pairs := [][]string{
+	defer db.out.Close()
+
+	data := []Data{
 		{"key1", "value1"},
 		{"key2", "value2"},
 		{"key3", "value3"},
 	}
 
-	outFile, err := os.Open(filepath.Join(dir, outFileName))
+	outPath := filepath.Join(dir, outFileName+"0")
+	outFile, err := os.Open(outPath)
+
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	t.Run("put/get", func(t *testing.T) {
-		for _, pair := range pairs {
-			err := db.Put(pair[0], pair[1])
+	t.Run("put&get check", func(t *testing.T) {
+		for i := 0; i < len(data); i++ {
+
+			key := data[i].key
+			value := data[i].value
+
+			err := db.Put(key, value)
 			if err != nil {
-				t.Errorf("Cannot put %s: %s", pairs[0], err)
+				t.Errorf("Cannot put %s: %s", key, err)
 			}
-			value, err := db.Get(pair[0])
+
+			result, err := db.Get(key)
 			if err != nil {
-				t.Errorf("Cannot get %s: %s", pairs[0], err)
+				t.Errorf("Cannot get %s: %s", key, err)
 			}
-			if value != pair[1] {
-				t.Errorf("Bad value returned expected %s, got %s", pair[1], value)
+
+			if result != value {
+				t.Errorf("Bad value returned expected %s, got %s", value, result)
 			}
 		}
 	})
@@ -53,40 +68,52 @@ func TestDb_Put(t *testing.T) {
 	}
 	size1 := outInfo.Size()
 
-	t.Run("file growth", func(t *testing.T) {
-		for _, pair := range pairs {
-			err := db.Put(pair[0], pair[1])
+	t.Run("test size", func(t *testing.T) {
+		for i := 0; i < len(data); i++ {
+			key := data[i].key
+
+			value := data[i].value
+
+			err := db.Put(key, value)
+
 			if err != nil {
-				t.Errorf("Cannot put %s: %s", pairs[0], err)
+				t.Errorf("Cannot put %s: %s", key, err)
 			}
+
 		}
+
 		outInfo, err := outFile.Stat()
 		if err != nil {
 			t.Fatal(err)
 		}
+
 		if size1*2 != outInfo.Size() {
-			t.Errorf("Unexpected size (%d vs %d)", size1, outInfo.Size())
+			t.Errorf("Unexpected size (%d != %d)", size1, outInfo.Size())
 		}
 	})
 
 	t.Run("new db process", func(t *testing.T) {
-		if err := db.Close(); err != nil {
+		if err := db.out.Close(); err != nil {
 			t.Fatal(err)
 		}
-		db, err = NewDb(dir)
+
+		db, err = NewDb(dir, 100)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		for _, pair := range pairs {
-			value, err := db.Get(pair[0])
+		for i := 0; i < len(data); i++ {
+			key := data[i].key
+			value := data[i].value
+
+			res, err := db.Get(key)
 			if err != nil {
-				t.Errorf("Cannot put %s: %s", pairs[0], err)
+				t.Errorf("Unable to get %s: %s", key, err)
 			}
-			if value != pair[1] {
-				t.Errorf("Bad value returned expected %s, got %s", pair[1], value)
+
+			if res != value {
+				t.Errorf("Expected value: %s, but received: %s", value, res)
 			}
 		}
 	})
-
 }
