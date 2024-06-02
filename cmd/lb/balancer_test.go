143b52427f80a511d0acb9c50507df08b32c5e63
Antonio <antony.dovzhenko@gmail.com>
2024-06-02 14:20:30 +0300

    Added health check in balancer test

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"os"
 	"testing"
-	"time"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func Test(t *testing.T) {
@@ -15,8 +14,6 @@ func Test(t *testing.T) {
 	suite.SetupSuite()
 	t.Run("TestBalancer", suite.TestBalancer)
 	t.Run("TestHealth", suite.TestHealth)
-	t.Run("TestTraffic", suite.TestGetSmallestTraffic)
-	t.Run("TestTraffic2", suite.TestGetSmallestTrafficTwo)
 }
 
 type TestSuite struct {
@@ -81,61 +78,16 @@ func (s *TestSuite) TestBalancer(t *testing.T) {
 	bytesServedLock.Unlock()
 }
 
-func (s *TestSuite) TestGetSmallestTraffic(t *testing.T) {
-	poolOfHealthyServers = []string{"server1:8080", "server2:8080", "server3:8080"}
-	bytesServed = map[string]int64{
-		"server1:8080": 100,
-		"server2:8080": 50,
-		"server3:8080": 150,
-	}
-
-	expectedIndex := 1
-	actualIndex := getSmallestTraffic()
-	assert.Equal(t, expectedIndex, actualIndex)
-}
-
-func (s *TestSuite) TestGetSmallestTrafficTwo(t *testing.T) {
-	poolOfHealthyServers = []string{"server1:8080", "server2:8080", "server3:8080"}
-	bytesServed = map[string]int64{
-		"server1:8080": 1488,
-		"server2:8080": 5252,
-		"server3:8080": 228,
-	}
-
-	expectedIndex := 2
-	actualIndex := getSmallestTraffic()
-	assert.Equal(t, expectedIndex, actualIndex)
-}
-
 func (s *TestSuite) TestHealth(t *testing.T) {
-	result := make([]string, len(s.serversPool))
-
-	server1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
-	defer server1.Close()
-
-	server2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
-	defer server2.Close()
-
-	parsedURL1, _ := url.Parse(server1.URL)
-	hostURL1 := parsedURL1.Host
-
-	parsedURL2, _ := url.Parse(server2.URL)
-	hostURL2 := parsedURL2.Host
-
-	servers := []string{
-		hostURL1,
-		hostURL2,
-		"server3:8080",
+	serverURL := os.Getenv("SERVER_URL")
+	if serverURL == "" {
+		serverURL = "http://localhost:8080"
 	}
 
-	healthCheck(servers, result)
-	time.Sleep(12 * time.Second)
-
-	assert.Equal(t, hostURL1, result[0])
-	assert.Equal(t, hostURL2, result[1])
-	assert.Equal(t, "", result[2])
+	resp, err := http.Get(serverURL + "/health")
+	if err != nil {
+		t.Fatalf("Failed to make request to %s: %s", serverURL+"/health", err)
+	}
+	defer resp.Body.Close()
+	assert.Equal(t, http.StatusOK, resp.StatusCode, "Health check failed for the main server")
 }
